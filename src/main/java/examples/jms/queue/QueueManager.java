06ca0b47e9b77f8a1b862487a1bf4006e70a1210
Artur Lomsadze <lomsadze@bpcbt.com>
2016-10-03 14:00:23 +0300

    Refactor JMS test

---
+++ src/main/java/examples/jms/queue/QueueManager.java
@@ -0,0 +1,71 @@
+package examples.jms.queue;
+
+import java.util.Hashtable;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class QueueManager {
+
+	private final static String JNDI_FACTORY = "weblogic.jndi.WLInitialContextFactory";
+
+	private final String jmsFactory;
+	private final String jmsQueue;
+	private final String url;
+
+	private QueueConnectionFactory qconFactory;
+	private QueueConnection qcon;
+	private QueueSession qsession;
+	private QueueSender qsender;
+	private QueueReceiver qReciever;
+	private Queue queue;
+	private TextMessage msg;
+
+	public QueueManager(String url, String jmsFactory, String jmsQueue) {
+		this.url = url;
+		this.jmsFactory = jmsFactory;
+		this.jmsQueue = jmsQueue;
+	}
+
+	public void init() throws NamingException, JMSException {
+		Hashtable<String, String> env = new Hashtable<>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+		env.put(Context.PROVIDER_URL, url);
+		InitialContext ctx = new InitialContext(env);
+
+		qconFactory = (QueueConnectionFactory) ctx.lookup(jmsFactory);
+		qcon = qconFactory.createQueueConnection();
+		qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+		queue = (Queue) ctx.lookup(jmsQueue);
+		qsender = qsession.createSender(queue);
+		qReciever = qsession.createReceiver(queue);
+		msg = qsession.createTextMessage();
+		qcon.start();
+	}
+
+	public void send(String message) throws JMSException {
+		msg.setText(message);
+		qsender.send(msg);
+	}
+
+	public Message recieve() throws JMSException {
+		return qReciever.receive(1000L);
+	}
+
+	public void close() throws JMSException {
+		qsender.close();
+		qsession.close();
+		qcon.close();
+	}
+}
--- src/test/java/examples/jms/queue/QueueRecieveTest.java
+++ src/test/java/examples/jms/queue/QueueRecieveTest.java
@@ -1,66 +1,31 @@
 package examples.jms.queue;
 
-import java.util.Hashtable;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Queue;
-import javax.jms.QueueConnection;
-import javax.jms.QueueConnectionFactory;
-import javax.jms.QueueReceiver;
-import javax.jms.QueueSender;
-import javax.jms.QueueSession;
-import javax.jms.Session;
 import javax.jms.TextMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.junit.Test;
 
 public class QueueRecieveTest {
 
-	public final static String JNDI_FACTORY = "weblogic.jndi.WLInitialContextFactory";
-	public final static String JMS_FACTORY = "jms/TestConnectionFactory";
-	public final static String QUEUE = "jms/TestDistributedQueue";
+	private static final String URL1 = "t3://localhost:7003";
+	private static final String URL2 = "t3://localhost:7004";
+	private static final String JMS_FACTORY = "jms/TestConnectionFactory";
+	private static final String QUEUE = "jms/TestDistributedQueue";
 
-	private QueueConnectionFactory qconFactory;
-	private QueueConnection qcon;
-	private QueueSession qsession;
-	private QueueSender qsender;
-	private QueueReceiver qReciever;
-	private Queue queue;
-	private TextMessage msg;
+	@Test
+	public void send() throws Exception {
+		QueueManager qm = new QueueManager(URL1, JMS_FACTORY, QUEUE);
+		qm.init();
+		qm.send("Test message 1");
+		qm.send("Test message 2");
+	}
 
 	@Test
 	public void recieve() throws Exception {
-		InitialContext ic = getInitialContext("t3://localhost:7004/");
-		init(ic, QUEUE);
-		Message message = qReciever.receive();
-		System.out.println(message);
+		QueueManager qm = new QueueManager(URL1, JMS_FACTORY, QUEUE);
+
+		qm.init();
+		System.out.println(((TextMessage) qm.recieve()).getText());
+		System.out.println(((TextMessage) qm.recieve()).getText());
 	}
 
-	private InitialContext getInitialContext(String url) throws NamingException {
-		Hashtable<String, String> env = new Hashtable<>();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
-		env.put(Context.PROVIDER_URL, url);
-		return new InitialContext(env);
-	}
-
-	public void init(Context ctx, String queueName) throws NamingException, JMSException {
-		qconFactory = (QueueConnectionFactory) ctx.lookup(JMS_FACTORY);
-		qcon = qconFactory.createQueueConnection();
-		qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-		queue = (Queue) ctx.lookup(queueName);
-		qsender = qsession.createSender(queue);
-		qReciever = qsession.createReceiver(queue);
-		msg = qsession.createTextMessage();
-		qcon.start();
-	}
-
-	public void close() throws JMSException {
-		qsender.close();
-		qsession.close();
-		qcon.close();
-	}
 }

45b8d5eff7f3edb917f36c502bc6abeb859180e7
Artur Lomsadze <lomsadze@bpcbt.com>
2017-03-06 09:04:15 +0300

    Added SSL for IBM MQ - 3

--- src/test/java/com/ail/test/jms/IBMMQSendTest.java
+++ src/test/java/com/ail/test/jms/IBMMQSendTest.java
@@ -1,15 +1,7 @@
 package com.ail.test.jms;
 
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
-import com.ibm.mq.jms.MQQueue;
-import com.ibm.mq.jms.MQQueueConnection;
-import com.ibm.mq.jms.MQQueueConnectionFactory;
-import com.ibm.mq.jms.MQQueueReceiver;
-import com.ibm.mq.jms.MQQueueSender;
-import com.ibm.mq.jms.MQQueueSession;
-import com.ibm.msg.client.wmq.WMQConstants;
+import com.ibm.mq.MQEnvironment;
+import com.ibm.mq.MQQueueManager;
 
 public class IBMMQSendTest {
 
@@ -22,49 +14,57 @@ public class IBMMQSendTest {
 	private static final String SSL_CIPHER_SUITE = "SSL_RSA_WITH_AES_256_CBC_SHA256";
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("javax.net.ssl.trustStore", "D:/Temp/mint/1/key.jks");
-		System.setProperty("javax.net.ssl.trustStore", "D:/Temp/mint/1/key.jks");
-		System.setProperty("javax.net.ssl.trustStorePassword", "mint");
+		System.setProperty("javax.net.ssl.trustStore", "D:/Temp/mint/APP1.jks");
+		System.setProperty("javax.net.ssl.keyStore", "D:/Temp/mint/APP1.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "mint");
 
-		MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
-		cf.setHostName(HOST_NAME);
-		cf.setPort(PORT);
+		MQEnvironment.hostname = HOST_NAME;
+		MQEnvironment.port = PORT;
+		MQEnvironment.channel = CHANNEL_NAME;
+		MQEnvironment.sslCipherSuite = SSL_CIPHER_SUITE;
 
-		cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+		MQQueueManager qMgr = new MQQueueManager(QUEUE_MANAGER_NAME);
 
-		cf.setQueueManager(QUEUE_MANAGER_NAME);
-		cf.setChannel(CHANNEL_NAME);
-
-		cf.setSSLCipherSuite(SSL_CIPHER_SUITE);
-		//		cf.setSSLFipsRequired(true);
-
-		//			MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection();
-		MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection(USERID, PASSWORD);
-
-		MQQueueSession session = (MQQueueSession) connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-		MQQueue queue = (MQQueue) session.createQueue("queue:///MQTestQueue");
-
-		MQQueueSender sender = (MQQueueSender) session.createSender(queue);
-		MQQueueReceiver receiver = (MQQueueReceiver) session.createReceiver(queue);
-
-		long uniqueNumber = System.currentTimeMillis() % 1000;
-
-		TextMessage message = session.createTextMessage("Basic Queue Test " + uniqueNumber);
-
-		// Start the connection
-		connection.start();
-		System.out.println("Sent message to Queue MQSenderQueue: " + message.getText());
-		sender.send(message);
-		System.out.println("Message Sent OK.\n");
-
-		System.out.println("Recieve message from Queue MQTestQueue...");
-		System.out.println("Message Recieved: " + ((TextMessage) receiver.receive(1000L)).getText());
-
-		sender.close();
-		receiver.close();
-
-		session.close();
-		connection.close();
+		//
+		//		MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
+		//		cf.setHostName(HOST_NAME);
+		//		cf.setPort(PORT);
+		//
+		//		cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+		//
+		//		cf.setQueueManager(QUEUE_MANAGER_NAME);
+		//		cf.setChannel(CHANNEL_NAME);
+		//
+		//		cf.setSSLCipherSuite(SSL_CIPHER_SUITE);
+		//		//		cf.setSSLFipsRequired(true);
+		//
+		//		//			MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection();
+		//		MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection(USERID, PASSWORD);
+		//
+		//		MQQueueSession session = (MQQueueSession) connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+		//		MQQueue queue = (MQQueue) session.createQueue("queue:///MQTestQueue");
+		//
+		//		MQQueueSender sender = (MQQueueSender) session.createSender(queue);
+		//		MQQueueReceiver receiver = (MQQueueReceiver) session.createReceiver(queue);
+		//
+		//		long uniqueNumber = System.currentTimeMillis() % 1000;
+		//
+		//		TextMessage message = session.createTextMessage("Basic Queue Test " + uniqueNumber);
+		//
+		//		// Start the connection
+		//		connection.start();
+		//		System.out.println("Sent message to Queue MQSenderQueue: " + message.getText());
+		//		sender.send(message);
+		//		System.out.println("Message Sent OK.\n");
+		//
+		//		System.out.println("Recieve message from Queue MQTestQueue...");
+		//		System.out.println("Message Recieved: " + ((TextMessage) receiver.receive(1000L)).getText());
+		//
+		//		sender.close();
+		//		receiver.close();
+		//
+		//		session.close();
+		//		connection.close();
 	}
 
 }

19d3b8fb951ee020fd893e1c367972954920ec19
Artur Lomsadze <lomsadze@bpcbt.com>
2017-03-06 14:54:01 +0300

    Refactor

--- src/test/java/com/ail/test/jms/IBMMQSSLSendTest.java
+++ src/test/java/com/ail/test/jms/IBMMQSSLSendTest.java
@@ -11,7 +11,7 @@ import com.ibm.mq.jms.MQQueueSender;
 import com.ibm.mq.jms.MQQueueSession;
 import com.ibm.msg.client.wmq.WMQConstants;
 
-public class IBMMQSendTest {
+public class IBMMQSSLSendTest {
 
 	private static final String HOST_NAME = "mint";
 	private static final String QUEUE_MANAGER_NAME = "MQTest";
@@ -19,8 +19,35 @@ public class IBMMQSendTest {
 	private static final int PORT = 1414;
 	private static final String USERID = "mquser";
 	private static final String PASSWORD = "mquser";
+	private static final String SSL_CIPHER_SUITE = "TLS_RSA_WITH_AES_128_CBC_SHA256";
 
 	public static void main(String[] args) throws Exception {
+		System.setProperty("javax.net.ssl.keyStore", "D:/Temp/mint/APP1.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "mint");
+		System.setProperty("javax.net.ssl.trustStore", "D:/Temp/mint/APP1.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "mint");
+
+		System.setProperty("javax.net.debug", "all");
+		System.setProperty("com.ibm.mq.cfg.useIBMCipherMappings", "false");
+		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+
+		// 1st option
+		//		MQEnvironment.hostname = HOST_NAME;
+		//		MQEnvironment.port = PORT;
+		//		MQEnvironment.channel = CHANNEL_NAME;
+		//		MQEnvironment.sslCipherSuite = SSL_CIPHER_SUITE;
+
+		// 2nd option
+		//		Hashtable<String, Object> mqht = new Hashtable<>();
+		//		mqht.put(CMQC.CHANNEL_PROPERTY, CHANNEL_NAME);
+		//		mqht.put(CMQC.HOST_NAME_PROPERTY, HOST_NAME);
+		//		mqht.put(CMQC.PORT_PROPERTY, PORT);
+		//		mqht.put(CMQC.SSL_CIPHER_SUITE_PROPERTY, SSL_CIPHER_SUITE);
+		//		mqht.put(CMQC.USER_ID_PROPERTY, USERID);
+		//		mqht.put(CMQC.PASSWORD_PROPERTY, PASSWORD);
+		//		MQQueueManager qMgr = new MQQueueManager(QUEUE_MANAGER_NAME, mqht);
+
+		// 3rd option
 		MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
 		cf.setHostName(HOST_NAME);
 		cf.setPort(PORT);
@@ -29,6 +56,9 @@ public class IBMMQSendTest {
 
 		cf.setQueueManager(QUEUE_MANAGER_NAME);
 		cf.setChannel(CHANNEL_NAME);
+
+		cf.setSSLCipherSuite(SSL_CIPHER_SUITE);
+		//		cf.setSSLFipsRequired(true); // not needed
 
 		//			MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection();
 		MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection(USERID, PASSWORD);
